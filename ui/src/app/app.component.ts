4d9e5bb50a316434a8649fee4c950f8c61df4662
galwort <wthomasgorbett@gmail.com>
2023-08-18 17:52:35 -0400

    Move bar to left

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  isCollapsed = true;
+  menuId = 'main-menu';
+
+  constructor(private menuCtrl: MenuController) {}
+
+  ngOnInit() {
+    this.menuCtrl.enable(true, this.menuId);
+    this.menuCtrl.open(this.menuId);
+  }
+
+  toggleMenu() {
+    this.isCollapsed = !this.isCollapsed;
+  }
 }
