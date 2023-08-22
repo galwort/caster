3c077aeadaf485df23b5a96b681414415f3a92fc
galwort <wthomasgorbett@gmail.com>
2023-08-21 20:46:20 -0400

    Move loading bar to home page

--- ui/src/app/pages/home/home.page.ts
+++ ui/src/app/pages/home/home.page.ts
@@ -18,6 +18,9 @@ export const db = getFirestore(app);
 export class HomePage {
   searchResults: Observable<any[]> = of([]);
   searchTerm: string = '';
+  progress = 0;
+  totalCalls = 0;
+  completedCalls = 0;
 
   constructor(private http: HttpClient, private router: Router) {}
 
@@ -38,7 +41,60 @@ export class HomePage {
       if (docSnapshot.exists()) {
         this.router.navigate(['/shows', showId]);
       } else {
-        this.router.navigate(['/loading', showId]);
+        const url = `http://localhost:8000/tv/${showId}`;
+        this.http.get<any>(url).subscribe(response => {
+          const showData = {
+            show_name: response.name,
+            show_image: response.poster_path,
+            show_first_air_date: response.first_air_date,
+            show_last_air_date: response.last_air_date,
+            show_seasons: response.number_of_seasons,
+            show_episodes: response.number_of_episodes,
+            show_overview: response.overview,
+          };
+          this.totalCalls = response.number_of_episodes; 
+          setDoc(doc(db, "shows", showId as string), showData).then(() => {
+            for (let seasonId = 1; seasonId <= response.number_of_seasons; seasonId++) {
+              const seasonUrl = `http://localhost:8000/tv/${showId}/season/${seasonId}`;
+              this.http.get<any>(seasonUrl).subscribe(seasonResponse => {
+                const seasonData = { season_episodes: seasonResponse.episodes.length };
+                const seasonDocRef = doc(db, "shows", showId as string, "seasons", seasonId.toString());
+                setDoc(seasonDocRef, seasonData).then(() => {
+                  for (let episodeId = 1; episodeId <= seasonResponse.episodes.length; episodeId++) {
+                    const episodeUrl = `http://localhost:8000/tv/${showId}/season/${seasonId}/episode/${episodeId}`;
+                    this.http.get<any>(episodeUrl).subscribe(episodeResponse => {
+                      const episodeData = {
+                        episode_name: episodeResponse.name,
+                        episode_air_date: episodeResponse.air_date,
+                        episode_overview: episodeResponse.overview,
+                      };
+                      const episodeDocRef = doc(db, "shows", showId as string, "seasons", seasonId.toString(), "episodes", episodeId.toString());
+                      setDoc(episodeDocRef, episodeData).then(() => {
+                        const castUrl = `http://localhost:8000/tv/${showId}/season/${seasonId}/episode/${episodeId}/credits`;
+                        this.http.get<any>(castUrl).subscribe(castResponse => {
+                          castResponse.cast.slice(0, 6).forEach((cast: any) => {
+                            const castData = {
+                              cast_name: cast.name,
+                              cast_image: cast.profile_path,
+                              cast_character: cast.character,
+                            };
+                            const castDocRef = doc(db, "shows", showId as string, "seasons", seasonId.toString(), "episodes", episodeId.toString(), "cast", cast.id.toString());
+                            setDoc(castDocRef, castData);
+                          });
+                          this.completedCalls++;
+                          this.progress = (this.completedCalls / this.totalCalls) * 100;
+                          if (this.completedCalls === this.totalCalls) {
+                            this.router.navigate(['/shows', showId]);
+                          }
+                        });                        
+                      });
+                    });
+                  }
+                });
+              });
+            }
+          });
+        });
       }
     });
   }
