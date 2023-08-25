ff7c0e811e071b46adae4f12373ecb750cdb1dbe
galwort <wthomasgorbett@gmail.com>
2023-08-24 20:42:14 -0400

    Update with references to interface and service

--- ui/src/app/pages/login/login.page.ts
+++ ui/src/app/pages/login/login.page.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from 'src/app/services/auth.service';
+import { LoginData } from 'src/app/interfaces/login-data.interface';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly router: Router
+  ) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit() {
+  login(loginData: LoginData) {
+    this.authService
+      .login(loginData)
+      .then(() => this.router.navigate(['/home']))
+      .catch((e) => console.log(e.message));
   }
-
 }
