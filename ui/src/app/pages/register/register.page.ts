2139c87be9e4034413db3d54a19b822ec1dfad2b
galwort <wthomasgorbett@gmail.com>
2023-08-26 15:59:45 -0400

    Implement register functionality

--- ui/src/app/pages/register/register.page.ts
+++ ui/src/app/pages/register/register.page.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { LoginData } from 'src/app/interfaces/login-data.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
 })
-export class RegisterPage implements OnInit {
+export class RegisterPage {
+  registerData: LoginData = { email: '', password: '' };
 
-  constructor() { }
+  constructor(
+    private readonly authService: AuthService,
+    private readonly router: Router
+  ) {}
 
   ngOnInit() {
   }
 
+  register() {
+    this.authService
+      .register(this.registerData)
+      .then(() => this.router.navigate(['/login']))
+      .catch((e) => console.log(e.message));
+  }
 }
