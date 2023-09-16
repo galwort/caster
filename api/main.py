57466e78b340dd4a3f64971e9b75ab291639d361
galwort <wthomasgorbett@gmail.com>
2023-09-15 21:20:51 -0400

    Fix issue of pulling credits in API call

--- api/main.py
+++ api/main.py
@@ -34,7 +34,7 @@ def get_tv(show_id: int):
 
 @app.get("/tv/{show_id}/credits")
 def get_tv(show_id: int):
-    url = f"https://api.themoviedb.org/3/tv/{show_id}?api_key={TMDB_API_KEY}"
+    url = f"https://api.themoviedb.org/3/tv/{show_id}/credits?api_key={TMDB_API_KEY}"
     response = get(url)
     return response.json()
 
@@ -48,7 +48,7 @@ def get_season(show_id: int, season_id: int):
 
 @app.get("/tv/{show_id}/season/{season_id}/credits")
 def get_season(show_id: int, season_id: int):
-    url = f"https://api.themoviedb.org/3/tv/{show_id}/season/{season_id}?api_key={TMDB_API_KEY}"
+    url = f"https://api.themoviedb.org/3/tv/{show_id}/season/{season_id}/credits?api_key={TMDB_API_KEY}"
     response = get(url)
     return response.json()
 
